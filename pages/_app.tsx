c6207ab4765d157149ab1f6a17eb4c8b25f73e61
Gregory Palacios <gregpalacios26@gmail.com>
2022-05-21 18:42:13 -0500

    Agregando pagos con paypal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from "next-auth/react";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { SWRConfig } from 'swr';
 import { AuthProvider, CartProvider, UiProvider } from '../context';
@@ -9,22 +10,24 @@ import { lightTheme } from '../themes';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider>
-      <SWRConfig
-        value={{
-          fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
-        }}
-      >
-        <AuthProvider>
-          <CartProvider>
-            <UiProvider>
-              <ThemeProvider theme={lightTheme}>
-                <CssBaseline />
-                <Component {...pageProps} />
-              </ThemeProvider>
-            </UiProvider>
-          </CartProvider>
-        </AuthProvider>
-      </SWRConfig>
+      <PayPalScriptProvider options={{ "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || '' }}>
+        <SWRConfig
+          value={{
+            fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+          }}
+        >
+          <AuthProvider>
+            <CartProvider>
+              <UiProvider>
+                <ThemeProvider theme={lightTheme}>
+                  <CssBaseline />
+                  <Component {...pageProps} />
+                </ThemeProvider>
+              </UiProvider>
+            </CartProvider>
+          </AuthProvider>
+        </SWRConfig>
+      </PayPalScriptProvider>
     </SessionProvider>
   )
 }
--- pages/orders/[id].tsx
+++ pages/orders/[id].tsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import { GetServerSideProps, NextPage } from 'next';
 import { getSession } from 'next-auth/react';
-import { Box, Card, CardContent, Divider, Grid, Typography, Chip } from '@mui/material';
+import { useRouter } from 'next/router';
+import { PayPalButtons } from "@paypal/react-paypal-js";
+import { Box, Card, CardContent, Divider, Grid, Typography, Chip, CircularProgress } from '@mui/material';
 import { CreditCardOffOutlined, CreditScoreOutlined } from '@mui/icons-material';
 import { ShopLayout } from '../../components/layouts';
 import { CartList, OrderSummary } from '../../components/cart';
 import { dbOrders } from '../../database';
 import { IOrder } from '../../interfaces';
+import { tesloApi } from '../../api';
+
+export type OrderResponseBody = {
+    id: string;
+    status:
+    | "COMPLETED"
+    | "SAVED"
+    | "APPROVED"
+    | "VOIDED"
+    | "PAYER_ACTION_REQUIRED";
+};
 
 interface Props {
     order: IOrder;
@@ -13,7 +27,32 @@ interface Props {
 
 const OrderPage: NextPage<Props> = ({ order }) => {
 
+    const router = useRouter();
     const { shippingAddress } = order;
+    const [isPaying, setIsPaying] = useState(false);
+
+    const onOrderCompleted = async (details: OrderResponseBody) => {
+        if (details.status !== 'COMPLETED') {
+            return alert('No hay pago realizado en Paypal');
+        }
+
+        setIsPaying(true);
+
+        try {
+            const { data } = await tesloApi.post(`/orders/pay`, {
+                transactionId: details.id,
+                orderId: order._id
+            });
+
+            if (data) {
+                router.reload();
+            }
+        } catch (error) {
+            setIsPaying(false);
+            console.log(error);
+            return alert('Error al procesar el pago en Paypal');
+        }
+    };
 
     return (
         <ShopLayout title='Resumen de la orden' pageDescription={'Resumen de la orden'}>
@@ -73,7 +112,14 @@ const OrderPage: NextPage<Props> = ({ order }) => {
                                 }}
                             />
 
-                            <Box sx={{ mt: 3 }} display="flex" flexDirection='column'>
+                            <Box display="flex"
+                                justifyContent="center"
+                                className='fadeIn'
+                                sx={{ display: isPaying ? 'flex' : 'none' }}>
+                                <CircularProgress />
+                            </Box>
+
+                            <Box flexDirection='column' sx={{ display: isPaying ? 'none' : 'flex', flex: 1 }} >
                                 {
                                     order.isPaid
                                         ? (
@@ -86,7 +132,24 @@ const OrderPage: NextPage<Props> = ({ order }) => {
                                             />
 
                                         ) : (
-                                            <h1>Pagar</h1>
+                                            <PayPalButtons
+                                                createOrder={(data, actions) => {
+                                                    return actions.order.create({
+                                                        purchase_units: [
+                                                            {
+                                                                amount: {
+                                                                    value: `${order.total}`,
+                                                                },
+                                                            },
+                                                        ],
+                                                    });
+                                                }}
+                                                onApprove={(data, actions) => {
+                                                    return actions.order!.capture().then((details) => {
+                                                        onOrderCompleted(details);
+                                                    });
+                                                }}
+                                            />
                                         )
                                 }
                             </Box>
